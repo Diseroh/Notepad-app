96b651579449e5cb4833c2f519864470d1689956
Diseroh <mihawskicx@gmail.com>
2023-12-20 18:44:33 +0100

    add api routes for Notes Actions

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require("express");
 const app = express();
 const { port } = require("./config.js");
 const apiRouters = require("./routes/api.js");
+const bodyParser = require("body-parser");
+
+//parsers Content-Type: application/json
+app.use(bodyParser.json());
 
 //routes
-app.use("/", apiRouters);
+app.use("/api", apiRouters);
 //data base initialization
 require("./db/mongoose.js");
 
 //serwer
 app.listen(port, () => {
-  console.log("serwer listening... http://localhost:" + port);
+  console.log(
+    "( ͡° ͜ʖ ͡°) ¯_( ͡° ͜ʖ ͡°)_/¯ serwer listening... http://localhost:" + port
+  );
 });
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const notesControls = require("../controls/api/notes.js");
+const notesControls = require("../controls/api/notesControls.js");
 
-router.get("/", notesControls.saveNote);
+//Save Note
+router.post("/notes", notesControls.saveNote);
+//Download all Notes
+router.get("/notes", notesControls.getAllNotes);
+//Download specific note
+router.get("/notes/:id", notesControls.getNote);
+//Edit Note
+router.put("/notes/:id", notesControls.updateNote);
+//Delete Note
+router.delete("/notes/:id", notesControls.deleteNote);
 
 module.exports = router;
